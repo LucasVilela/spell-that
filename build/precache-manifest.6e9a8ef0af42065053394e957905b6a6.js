830fda1093d8d4f80c690b98c8f5bbf63db6ac40
Lucas Vilela <fleminglucas@gmail.com>
2020-09-11 17:19:03 +1000

    test

---
+++ build/precache-manifest.6e9a8ef0af42065053394e957905b6a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0517f645f4c6c3cdd78326f071d08a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3126736264cac31e0bef",
+    "url": "/static/css/main.af6edbab.chunk.css"
+  },
+  {
+    "revision": "06119e33fab45d903436",
+    "url": "/static/js/2.f9d82fd4.chunk.js"
+  },
+  {
+    "revision": "3126736264cac31e0bef",
+    "url": "/static/js/main.d4fd5bea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
